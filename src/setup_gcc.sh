196b58f9f08ed58078c7f99a3635a841dec03a77
Andrew Jewett <jewett.aij@gmail.com>
2019-10-12 22:20:22 -0700

    removed some cruft from "setup_gcc.sh"

--- src/setup_gcc.sh
+++ src/setup_gcc.sh
@@ -6,11 +6,7 @@ export L_COMP="ar rs"
 
 export LFLAGS="-fopenmp"  #-static
 
-# Note: I get strange behavior ("Heisenbug") if I use optimizations (-O3)
-# COMMENTING OUT:
-# export MY_FLAGS="-std=c++11 -O3 -DNDEBUG -ffast-math"
-# INSTEAD USE:
-export MY_FLAGS="-std=c++11 -DNDEBUG -ffast-math"
+export MY_FLAGS="-std=c++11 -O3 -DNDEBUG -ffast-math"
 export CFLAGS="-c $MY_FLAGS -fopenmp"
 export CPP_PRELINKER_COMMAND="echo"
 export COMPILER_TEMP_FILES=""
